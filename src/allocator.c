a09c0feb24fb500bee05ebe87e09cd8b8a492424
Sanket Bharadwaj <sanketbharadwaj15@gmail.com>
2026-02-11 22:57:41 +0530

    Add stati heap memory

--- src/allocator.c
+++ src/allocator.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <stddef.h>
+
+#define HEAP_SIZE (1024 * 1024 * 10)
+
+static unsigned char heap[HEAP_SIZE];
+static unsigned char *heap_ptr = heap;
+
 int main(){
 	printf("allocator starting...\n");
+	printf("Heap starts at: %p\n",heap);
+	printf("Heap ends at:   %p\n",heap + HEAP_SIZE);
 	return 0;
 }
