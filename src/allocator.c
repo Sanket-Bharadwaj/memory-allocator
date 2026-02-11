a15d2c80abcc2904dd509bc1c0484f2a1ceea169
Sanket Bharadwaj <sanketbharadwaj15@gmail.com>
2026-02-11 23:10:46 +0530

    Working bump allocator

--- src/allocator.c
+++ src/allocator.c
@@ -6,9 +6,33 @@
 static unsigned char heap[HEAP_SIZE];
 static unsigned char *heap_ptr = heap;
 
+void* vyom_malloc(size_t size) {
+
+    // align to 8 bytes
+    size = (size + 7) & ~7;
+
+    // check capacity
+    if (heap_ptr + size > heap + HEAP_SIZE) {
+        return NULL;
+    }
+
+    void* ptr = heap_ptr;  // save current position
+    heap_ptr += size;      // move forward
+
+    return ptr;
+}
+
 int main(){
-	printf("allocator starting...\n");
-	printf("Heap starts at: %p\n",heap);
-	printf("Heap ends at:   %p\n",heap + HEAP_SIZE);
-	return 0;
+
+    printf("allocator starting...\n");
+
+    int *a = vyom_malloc(sizeof(int));
+    double *b = vyom_malloc(sizeof(double));
+    char *c = vyom_malloc(1);
+
+    printf("a: %p\n", a);
+    printf("b: %p\n", b);
+    printf("c: %p\n", c);
+
+    return 0;
 }
